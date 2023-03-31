a9d6dcb5acbc3ed88ddf72656e5a8dbacd259476
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-31 16:50:40 +0200

    init commit

--- database/migrations/2023_03_31_144517_create_songs_table.php
+++ database/migrations/2023_03_31_144517_create_songs_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('songs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('title');
+            $table->string('album');
+            $table->string('author');
+            $table->string('editor')->nullable();
+            $table->float('lenght');
+            $table->text('poster');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('songs');
     }
 };
